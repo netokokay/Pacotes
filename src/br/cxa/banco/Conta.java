8f78fba10eee19344b115cb8c3380f39685c9367
Neto Kokay <netokokay@gmail.com>
2022-08-09 23:01:53 -0300

    Pacotes

---
+++ src/br/cxa/banco/Conta.java
@@ -0,0 +1,24 @@
+package br.cxa.banco;
+
+public class Conta {
+	
+	private int conta;
+	private String nome;
+	
+	
+	public int getConta() {
+		return conta;
+	}
+	public void setConta(int conta) {
+		this.conta = conta;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+
+}
---
+++ src/br/gov/empresa/ContaPublica.java
@@ -0,0 +1,34 @@
+package br.gov.empresa;
+
+public class ContaPublica {
+
+	private String responsavel;
+	private int conta;
+	private int	telefone;
+	
+	
+	public String getResponsavel() {
+		return responsavel;
+	}
+	public void setResponsavel(String responsavel) {
+		this.responsavel = responsavel;
+	}
+	public int getConta() {
+		return conta;
+	}
+	public void setConta(int conta) {
+		this.conta = conta;
+	}
+	public int getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(int telefone) {
+		this.telefone = telefone;
+	}
+	
+	
+	
+	
+	
+	
+}
